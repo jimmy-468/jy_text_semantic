7949d6764011bed9bfd1b6da37cb4b9c586ad476
jimmy-468 <73893988+jimmy-468@users.noreply.github.com>
2024-11-27 10:11:55 +0100

    bugfixes (now finds pos words file, verbose output)

--- R/jy_pos_neg_words.R
+++ R/jy_pos_neg_words.R
@@ -11,11 +11,15 @@
 
 jy_pos_neg_words <- function(words_from_text_file){
 
+  # Bugfix: FILE paths to reference text files containing positive and neg words
+  filepath_pos_words = system.file("data", "Positive words.txt", package = "JyTextSemantic")
+  filepath_neg_words = system.file("data", "Negative words.txt", package = "JyTextSemantic")
+
   # READ positive and negative word vector, assume one word per row in word-list-file
   # their file paths are hardcoded, sorry
   # output from read.delim() is a list so unlist() to turn output to char vector
-  pos_words <- unlist(read.delim(file="../data/Positive words.txt", header = FALSE))
-  neg_words <- unlist(read.delim(file="../data/Negative words.txt", header = FALSE))
+  pos_words <- unlist(read.delim(file = filepath_pos_words, header = FALSE))
+  neg_words <- unlist(read.delim(file = filepath_neg_words, header = FALSE))
 
   # Create placeholder char vectors for matches
   pos_words_matches = c() #empty word vector for positive words matches
@@ -45,4 +49,9 @@ jy_pos_neg_words <- function(words_from_text_file){
   ratio_pos_words <- n_pos_words_found/(n_pos_words_found + n_neg_words_found)
   return(ratio_pos_words)
   # we can return other variables here of course, perhaps even in organized in an obj
+
+  #VERBOSE reporting of results
+  print(paste("\n Positive words found:", as.character(n_pos_words_found)))
+  print(paste("\n Negative words found:", as.character(n_neg_words_found)))
+  print(paste("\n Ratio positive/negative words:", as.character(ratio_pos_words)))
 }
